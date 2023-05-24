d746f06318aecb041f8137ab6afab24dfc21ccdc
Karin Oliveira <131598814+karindefatima@users.noreply.github.com>
2023-05-24 18:10:37 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,8 @@
+import streamlit as st
 
+uploaded_file = st.file_uploader("Escolher Imagem")
+option = st.selectbox(
+    'Qual arquitetura será utilizada na identificação?',
+    ('YOLO', 'CNN', 'SSD'))
+
+st.write('Identificar objetos usando:', option)
